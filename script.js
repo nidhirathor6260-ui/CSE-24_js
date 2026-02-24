47a7eb71899fd6e5e507bbf6d00bebba1893362b
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-02-24 15:39:38 +0530

    if else & unique identification

--- script.js
+++ script.js
@@ -29,3 +29,41 @@ console.log(typeof(num));
 
 num = "Nidhi";
 console.log(typeof(num));
+
+//Array-[]; function; object-{}
+
+let sayHello = function(){
+    alert('hello world');
+}
+
+console.log(typeof(sayHello));
+
+let numArray = [1, 2, 3];
+
+let animals = new Array('cat', 'dog', 'mouse', 'lion');
+
+console.log(typeof(numArray));
+
+//new allocate dynamic memory;
+
+let person = {
+    'Nidhi': 15,
+    'nisha': 18,
+    'Neha': 21
+}
+
+console.log(typeof(person));
+
+const sym1 = Symbol(4);
+console.log(sym1);
+
+const sym2 = Symbol(4);
+console.log(sym2);
+
+if(sym1 == sym2){
+    console.log("true");
+}
+
+else{
+    console.log('false');
+}
