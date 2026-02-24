3e624f3fe817da17d6a3d282dc54ec04e64e8bde
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-02-24 15:49:05 +0530

    double equal to and triple equal to

---
+++ equalto.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = "10";
+
+if(x==y){   //compares only values of variable;
+    console.log('true');
+}else {
+    console.log('false')
+}
+
+if(x===y){   //compares both values as well as data type;
+    console.log('true');
+}else{
+    console.log('false');
+}
