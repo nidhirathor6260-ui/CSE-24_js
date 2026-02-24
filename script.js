5614a282010a0940d97d3560fdf0d797f45e2ade
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-02-24 15:23:17 +0530

    Understand different types of datatype

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 let name = "Nidhi";
 console.log(name);
 document.write("I am Nidhi");
+
+
+let x;
+console.log(typeof(x));
+
+let  y= null;
+console.log(typeof(y));
+
+// null is a value given to avoid garbage value;
+// undefined when type is not defined;
+// local variable - let , const;
+
+const a = 10;
+
+// const initialize at the time of declaration
+
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+
+num = true;
+console.log(typeof(num));
+
+num = undefined;
+console.log(typeof(num));
+
+num = "Nidhi";
+console.log(typeof(num));
