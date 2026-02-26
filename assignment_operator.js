73bebdab70d121276cf0d8b8fd06bf840ea88d79
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-02-26 22:08:04 +0530

    Assignment operator done

---
+++ assignment_operator.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 20;
+
+console.log('a=',a ,' b=',b);
+
+result = (a=b);
+console.log(result);
+
+result = (a>b);
+
+console.log(result);
+
+result = (a>=b);
+console.log(result);
+
+result = (a<=b);
+console.log(result);
