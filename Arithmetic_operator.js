02dd07077b089926e3cd611b03bdf13b4376045f
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-02-26 22:07:42 +0530

    Arithmetic operator done

---
+++ Arithmetic_operator.js
@@ -0,0 +1,18 @@
+
+let a = 10 , b = 20 ;
+
+
+let result = a+b;
+console.log('a+b= ',result);
+
+result = a-b;
+console.log('a-b= ',result);
+
+result = a*b;
+console.log('a*b= ',result);
+
+result = a/b;
+console.log('a/b= ',result);
+
+result = a%b;
+console.log('a%b= ',result);
