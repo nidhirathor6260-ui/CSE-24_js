545f679fbb24db7860f5eec381255ca5c3cd253e
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-02-26 22:15:57 +0530

    max of three using ternary operator

---
+++ maxOfThree.js
@@ -0,0 +1,8 @@
+
+let x = 10;
+let y = 20;
+let z = 30;
+
+console.log('x = ',x , ' y = ', y , ' z = ', z);
+console.log('Maximum Number = ');
+(x>y) ? ((x>z) ? console.log(x) : console.log(z)): ((y>z)? console.log(y): console.log(z));
