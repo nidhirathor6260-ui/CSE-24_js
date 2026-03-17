fd30ad6b6de0a29985793242127b7ae0785b1034
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-03-17 15:34:55 +0530

    Object done successsfully

---
+++ object.js
@@ -0,0 +1,15 @@
+let person = {
+    name : "Nidhi",
+    course : "B.tech",
+    age : 19,
+    isMarried : false
+}
+
+
+let person2 = new Object();
+person2.name = 'Nisha';
+person2.age = 15;
+person2.isMarried = false;
+person2.address = {};
+person2.address.street = 'Kailash nagar';
+person2['address']['flatNo'] = 33;
