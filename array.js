676adb2399ce03df1fcd07c799162c8d7d7d9e0c
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-03-17 15:34:41 +0530

    Array done

---
+++ array.js
@@ -0,0 +1,13 @@
+let myInfo = new Array();
+let myInfo1 = Array();
+
+console.log(myInfo, myInfo1);
+
+let collection = [
+
+]
+
+collection.teacherName = 'Saswati Swain';
+console.log(collection.length);
+console.log(collection[0]); 
+console.log(collection.length - 1);
