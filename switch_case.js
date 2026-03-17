58c5e8ec1c9a3a5b778b09141963366222eb8284
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-03-17 15:17:59 +0530

    switch case statements

---
+++ switch_case.js
@@ -0,0 +1,15 @@
+let grade = input("Enter grade: ");
+
+switch(grade){
+    
+    case 'A': console.log("Good job");
+              break;
+    case 'B': console.log("Pretty good");
+             break;
+    case 'C': console.log("passed");
+    break;
+    case 'D': console.log("bad");
+    break;
+    default : console.log("Failed");
+
+}
