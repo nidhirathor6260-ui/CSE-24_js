4c3370fca5d730f0b580f3bcd5a22ccf3e2225ca
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-03-18 20:58:42 +0530

    all mouse and keyboard events done successfully

---
+++ Events/script.js
@@ -0,0 +1,31 @@
+function OnClick(){
+    alert('Button clicked');
+}
+
+function OnMouseOver(){
+    alert('Mouse over');
+}
+
+function OnMouseLeave(){
+    alert('Mouse leave');
+}
+
+function onDblclick(){
+    alert('Double clicked');
+}
+
+function onMousedown(){
+    alert('Mouse down');
+}
+
+function onkeydown(){
+    alert('key down pressed');
+}
+
+function onkeypress(){
+    alert('key pressed');
+}
+
+function onkeyUP(){
+    alert('Key up pressed');
+}
